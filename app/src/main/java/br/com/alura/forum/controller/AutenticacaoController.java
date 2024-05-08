1f8d2047acffb8988466b85bc435083527772f8b
oliveira-italo <arievilo.olati@gmail.com>
2024-05-08 11:47:29 -0300

    custom metrics

--- app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
+++ app/src/main/java/br/com/alura/forum/controller/AutenticacaoController.java
@@ -1,7 +1,10 @@
 package br.com.alura.forum.controller;
 
-import javax.validation.Valid;
-
+import br.com.alura.forum.config.security.TokenService;
+import br.com.alura.forum.controller.dto.TokenDto;
+import br.com.alura.forum.controller.form.LoginForm;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
@@ -14,34 +17,56 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.config.security.TokenService;
-import br.com.alura.forum.controller.dto.TokenDto;
-import br.com.alura.forum.controller.form.LoginForm;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
 @Profile(value = {"prod", "test"})
 public class AutenticacaoController {
-	
-	@Autowired
-	private AuthenticationManager authManager;
-	
-	@Autowired
-	private TokenService tokenService;
-	
-	@PostMapping
-	public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
-		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
-		
-		try {
-			Authentication authentication = authManager.authenticate(dadosLogin);
-			String token = tokenService.gerarToken(authentication); 		
-			return ResponseEntity.ok(new TokenDto(token, "Bearer"));
-			
-		} catch (AuthenticationException e) {
-			return ResponseEntity.badRequest().build();
-		}
 
-		
-	}
+    private final Counter authUserSuccess;
+    private final Counter authUserError;
+
+    @Autowired
+    private AuthenticationManager authManager;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public AutenticacaoController(MeterRegistry registry) {
+        /**
+         * custom metrics
+         */
+        this.authUserSuccess = Counter
+                .builder("auth_user_success")
+                .description("usuarios autenticados")
+                .register(registry);
+
+        this.authUserError = Counter
+                .builder("auth_user_error")
+                .description("erros de login")
+                .register(registry);
+    }
+
+    @PostMapping
+    public ResponseEntity<TokenDto> autenticar(@RequestBody @Valid LoginForm form) {
+        UsernamePasswordAuthenticationToken dadosLogin = form.converter();
+
+        try {
+            Authentication authentication = authManager.authenticate(dadosLogin);
+            String token = tokenService.gerarToken(authentication);
+
+            authUserSuccess.increment();
+
+            return ResponseEntity.ok(new TokenDto(token, "Bearer"));
+
+        } catch (AuthenticationException e) {
+
+            authUserError.increment();
+
+            return ResponseEntity.badRequest().build();
+        }
+
+
+    }
 }
